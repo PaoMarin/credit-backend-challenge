c727f45314bdb0216e6034ce98b4447d58b4a611
PaoMarin <paomg.9614@gmail.com>
2020-12-16 18:57:49 -0600

    Add test for send emails

--- test/mailers/previews/information_mailer_preview.rb
+++ test/mailers/previews/information_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/information_mailer
 class InformationMailerPreview < ActionMailer::Preview
   def send_client_information_preview
-    InformationMailer.send_client_information(Client.first)
+    InformationMailer.send_client_information(client_id: 30,calculation: 200)
   end
 end
