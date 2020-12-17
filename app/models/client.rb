7e2d6fc3f0e59b299a56ad76b50059f791df61a5
PaoMarin <paomg.9614@gmail.com>
2020-12-16 18:55:39 -0600

    Add model with method and validations create client

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,5 +4,13 @@ class Client < ApplicationRecord
 
    validates :amount, numericality:  {only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 2000, message: 'must be number a between 100 & 2000'}
 
+   # Method calculate loan amount per month
+   def self.fee_calculation(client)
+      @calculation = ((client.amount * 0.18) / (1 - (1 + 0.18) ** - client.paytime))
+   end
 
+   # Method of dividing the payment amount in case of being biweekly
+   def self.biweekly_payment(calculation)
+      @calculation =  (calculation / 2)
+   end
 end
