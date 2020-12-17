68bd2754901bb84bcffe9053fb8f2ddae8411b6b
PaoMarin <paomg.9614@gmail.com>
2020-12-16 18:56:19 -0600

    Add format html/text for send email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'credit.challenge.app@gmail.com'
+  default from: "credit.challenge.app@gmail.com"
   layout 'mailer'
 end
--- app/mailers/information_mailer.rb
+++ app/mailers/information_mailer.rb
@@ -1,10 +1,12 @@
 class InformationMailer < ApplicationMailer
   default from: "credit.challenge.app@gmail.com"
 
-  def send_client_information(client)
-    @client = client
-    @mail = "paomg.9614@gmail.com"
-    mail(to: @client.email, subject: 'Request Credit Information')
+  def send_client_information(params)
+    client_id = params[:client_id]
+    @calculation = params[:calculation]
+    @client = Client.find(client_id)
+    email = 'paomg.9614@gmail.com'
+    mail(to: email, subject: 'Request Credit Information')
   end
 
 end
