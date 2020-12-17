e6d954ee058b00b558aa1c9bd26ca62c79b609ef
PaoMarin <paomg.9614@gmail.com>
2020-12-16 18:55:03 -0600

    Add controller with method create

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,56 +1,43 @@
 class ClientsController < ApplicationController
+  # GET /clients
+  def index
+    @clients = Client.all
+    render json: @clients
+  end
 
-        before_action :set_client, only: [:show, :update, :destroy]
-
-        # GET /clients
-        def index
-          @clients = Client.all
-
-          render json: @clients
-        end
-
-        # GET /clients/1
-        def show
-          render json: @client
-        end
-
-        # POST /clients
-        def create
-          @client = Client.new(client_params)
-          if @client.save
-            @calculation = helpers.fee_calculation(@client)
-            if(@client.frecuency == 2)
-              @calculation = helpers.biweekly_payment(@calculation)
-            end
-            InformationMailer.send_client_information(client: @client).deliver_now
-            render json: @client, status: :created, location: @client
-          else
-            render json: @client.errors, status: :unprocessable_entity
-          end
-        end
-
-        # PATCH/PUT /clients/1
-        def update
-          if @client.update(client_params)
-            render json: @client
-          else
-            render json: @client.errors, status: :unprocessable_entity
-          end
-        end
-
-        # DELETE /clients/1
-        def destroy
-          @client.destroy
-        end
-
-        private
-          # Use callbacks to share common setup or constraints between actions.
-          def set_client
-            @client = Client.find(params[:id])
-          end
-
-          # Only allow a trusted parameter "white list" through.
-          def client_params
-            params.require(:client).permit(:name, :email, :totalIngress, :sector, :workYears, :amount, :frecuency, :paytime)
-          end
+  # POST /clients
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      @result = Client.fee_calculation(@client)
+      if(@client.frecuency == 2)
+        @result = Client.biweekly_payment(@result)
       end
+      # Round the amount of the couta
+      @calculation = @result.round(2)
+      # Send email with client information
+      InformationMailer.send_client_information(client_id: @client.id, calculation: @calculation).deliver_now
+      render json: {
+        "status": "success",
+        "code": 200,
+        "message": "The client would pay a couta of:",
+        "data": [
+          {
+            "amount": @client.amount,
+            "text": @calculation,
+            "frecuency": @client.frecuency,
+            "payTime": @client.paytime,
+          }
+        ]
+      }
+    else
+      render json: @client.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  # Only allow a trusted parameter "white list" through.
+  def client_params
+    params.require(:client).permit(:name, :email, :totalIngress, :sector, :workYears, :amount, :frecuency, :paytime)
+  end
+end
