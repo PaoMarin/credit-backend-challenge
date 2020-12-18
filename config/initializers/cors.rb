55981c52d82ef588455a15e4e29f443aaace502c
PaoMarin <paomg.9614@gmail.com>
2020-12-17 20:35:21 -0600

    Changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,10 +4,10 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-# IMPORTANT: For purposes of running locally, change the origin to the local path of the project. For example: http://localhost:3000
+# IMPORTANT: For purposes of running locally, change the origin to the local path of the project. For example: http://localhost:8080
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://credit-challenge-6b4d9.web.app/'
     resource '*',
              :headers => :any,
              :methods => [:get, :post, :delete, :put, :options]
